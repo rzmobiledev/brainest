1756f0c8ca16a4510214e3e121144a33efeaa222
Only Rizal <safrizal@loratechai.com>
2023-04-07 12:21:33 +0700

    new updates

--- hangman.py
+++ hangman.py
@@ -1,74 +1,67 @@
-"""
+'''
 The hangman game is a word guessing game where the player is given a word and has to guess the letters that make up the word. 
 The player is given a certain number of tries (no more than 6 wrong guesses are allowed) to guess the correct letters before the game is over.
-"""
+'''
 
 # Output
-"""
+'''
 You have 6 tries left.                                                                                                                                           
 Used letters:                                                                                                                                                    
 Word: _ _ _ _                                                                                                                                                    
 Guess a letter: a 
+
 You have 6 tries left.                                                                                                                                           
 Used letters: a                                                                                                                                                  
 Word: _ a _ a                                                                                                                                                    
 Guess a letter: j    
+
 You have 6 tries left.                                                                                                                                           
 Used letters: j a                                                                                                                                                
 Word: j a _ a                                                                                                                                                    
 Guess a letter: v                                                                                                                                                
 You guessed the word java !
-"""
+'''
 
-stored_letters = list("java")  # converts string to list
-guessed_word = len(stored_letters) * ["_"]
-used_letters = list()
-
-
+count = 0
 count_balance = 6
 
-while count_balance <= 6:
-    try:
-        print("\nYou have {} tries left.".format(count_balance))
-        print("Used letters: {}".format(" ".join(used_letters)))
-        print("word: {}".format(" ".join(guessed_word)))
-        guess = input("Guess a letter: ")
-
-        # if input is multiple character, it returns warrning message with no change in count_balance
-        if len(guess) > 1:
-            print("Only one char is allowed")
-            continue
-
-        # decrements the no.of tries if the guess is wrong
-        if guess not in stored_letters:
-            count_balance -= 1
-            if count_balance < 1:
-                break
-            used_letters.append(guess)
-
-        # checks each letter in stored letters with the guessed letter
-        else:
-            used_letters.append(guess)
-
-            for i in range(len(stored_letters)):
-                # accepts both lowercase and uppercase inputs
-                if (
-                    stored_letters[i] == guess
-                    or stored_letters[i] == guess.upper()
-                    or stored_letters[i] == guess.lower()
-                ):
-                    guessed_word[i] = stored_letters[i]
-
-        if guessed_word == stored_letters:
-            print("You guessed the word {} !".format("".join(guessed_word)))
-            break
-
-    except Exception as e:
-        print(str(e))
+letters = list('java')
+word = ["_"] * 4
 
 
-# return the messeage if the guess is incorrect
-if guessed_word != stored_letters:
-    print("Wrong Guess")
+while count < 6:
+    print("You have {} tries left".format(count_balance))
+    print("Used letters: {}".format("_".join(word)))
+    print("Word: {}".format("_ ".join(word)))
+    guess = input("Guess a letter: ")  
 
+    # Limit guess letter only one char
+    if len(guess) > 1:
+        print("Only one char is allowed!")
+        continue
+
+    if guess not in letters:
+        # start to count every loop
+        count += 1
+        # We start to minus 1 each count balance
+        count_balance -= 1
+
+    # Check index of letters
+    for i in range(len(letters)):
+        if letters[i] == guess:
+            # add used letters to list
+            word[i] = letters[i]
+        
+    if word == letters:
+        print("You guessed the word {} !".format("".join(word)))
+        break
+    
+    # if above condition does not meet,
+    # we need to continue the program
+    continue
+
+# check if guessed word is false
+if word != letters:
+    print("Wrong Guess!")
+    
 print("You have exited the game")
--- test.py
+++ test.py
@@ -1,54 +1,42 @@
-stored_letters = list("java")  # converts string to list
-guessed_word = len(stored_letters) * ["_"]
-used_letters = list()
+from random import randint
+
+WORDS = ["tree", "car", "lordoftherings", "godzilla", "disestablishmentarianism", "kapow", "java", "foobar"]
+
+def get_indices(letter: str, word: str):
+    indices = []
+    for i in range(len(word)):
+        if word[i] == letter:
+            indices.append(i)
+    return indices
 
 
-count_balance = 6
+word = WORDS[randint(0, len(WORDS))]
 
-while count_balance <= 6:
-    try:
-        print("\nYou have {} tries left.".format(count_balance))
-        print("Used letters: {}".format(" ".join(used_letters)))
-        print("word: {}".format(" ".join(guessed_word)))
-        guess = input("Guess a letter: ")
+guess = ["_"] * len(word)
 
-        # if input is multiple character, it returns warrning message with no change in count_balance
-        if len(guess) > 1:
-            print("Only one char is allowed")
-            continue
-
-        # decrements the no.of tries if the guess is wrong
-        if guess not in stored_letters:
-            count_balance -= 1
-            if count_balance < 1:
-                break
-            used_letters.append(guess)
-
-        # checks each letter in stored letters with the guessed letter
-        else:
-            used_letters.append(guess)
-
-            for i in range(len(stored_letters)):
-
-                # accepts both lowercase and uppercase inputs
-                if (
-                    stored_letters[i] == guess
-                    or stored_letters[i] == guess.upper()
-                    or stored_letters[i] == guess.lower()
-                ):
-                    
-                    guessed_word[i] = stored_letters[i]
-
-        if guessed_word == stored_letters:
-            print("You guessed the word {} !".format("".join(guessed_word)))
-            break
-
-    except Exception as e:
-        print(str(e))
-
-
-# return the messeage if the guess is incorrect
-if guessed_word != stored_letters:
-    print("Wrong Guess")
-
-print("You have exited the game")
+tries_left = 6
+used_letters = set()
+    
+while tries_left > 0:
+    print("You have", tries_left, "tries left.")
+    print("Used letters:", " ".join(used_letters))
+    print("Word:", " ".join(guess))
+    print()
+    
+    user_letter = input("Guess a letter: ")
+    used_letters.add(user_letter)
+    guessed = get_indices(user_letter, word)
+    if not guessed:
+        tries_left -= 1
+        continue
+        
+    for i in guessed:
+        guess[i] = user_letter
+    
+    if "".join(guess) == word:
+        print("You guessed the word " + word + "!")
+        break
+        
+if "".join(guess) != word:
+    print("You didn't guess the word :/")
+    print("It was:", word)
